f693c9e0dba31311f5e9bacce551ccaa6ef8868e
Pixscl <robinartb@gmail.com>
2023-07-14 20:00:42 +0800

    with database connection database name dms table name user

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Document_Management_System_with_UI
 {
@@ -24,24 +25,54 @@ namespace Document_Management_System_with_UI
             InitializeComponent();
         }
 
+        private void Username_TextBox(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=ra05182002");
+
+
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void Register_Button_Click(object sender, RoutedEventArgs e)
         {
+            string insertQuery = "INSERT INTO dms.user(username,password) VALUES ('" + Username.Text + "'," + Passwordtextbox.Text + ")";
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(insertQuery, connection);
+            if(command.ExecuteNonQuery()==1) 
+            {
+                MessageBox.Show("Register Success");
+            }
+            else
+            {
+                MessageBox.Show("Register Failed");
+            }
 
+            connection.Close();
         }
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        private void Login_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            MainWindow objMainWindow = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objMainWindow.Show();
+        }
+        private void ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            Passwordtextbox.Text = Password.Password;
+            Password.Visibility = Visibility.Collapsed;
+            Passwordtextbox.Visibility = Visibility.Visible;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ShowPassword_Unchecked(object sender, RoutedEventArgs e)
         {
-
+            Password.Password = Passwordtextbox.Text;
+            Passwordtextbox.Visibility = Visibility.Collapsed;
+            Password.Visibility = Visibility.Visible;
         }
     }
 }
